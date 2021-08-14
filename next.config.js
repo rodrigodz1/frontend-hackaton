4eecb6dafe7c7ec864ed2d46f33ab4ec8c028002
Rodrigo Brito <spensmusic@gmail.com>
2021-08-14 13:52:10 -0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    localAPI: "http://localhost:7071",
+  },
+  assetPrefix:
+    "https://cdn.jsdelivr.net/npm/@aspnet/signalr@1.1.2/dist/browser/signalr.js",
+  reactStrictMode: true,
+};
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import { Title } from "../styles/styled";
+
+export default function Home() {
+  return (
+    <div>
+      <Title>mensagem inicial</Title>
+      <h1>teste</h1>
+    </div>
+  );
+}
